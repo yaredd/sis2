f79caaf989e4e683606339b0ab6b1cd3aa1077f0
Yared Deneke <yared.deneke@icsaddis.edu.et>
2011-12-17 17:40:44 +0300

    live version

--- app/models/extended_grade.rb
+++ app/models/extended_grade.rb
@@ -1,6 +1,6 @@
 class ExtendedGrade < ActiveRecord::Base
   belongs_to :grade
   belongs_to :benchMark
-  validates_inclusion_of :bmGrade, :in => %w(7 6 5 4 3 2 1 NG), :allow_blank => true
+  validates_inclusion_of :bmGrade, :in => %w(7 6 5 4 3 2 1 7M 6M 5M 4M 3M 2M 1M INC P F NG), :allow_blank => true
 
 end
