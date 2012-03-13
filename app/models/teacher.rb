349d19c6385fc597a1a48d5cdfd0a6ef2e2592f8
Yared Deneke <yared.deneke@icsaddis.edu.et>
2012-03-13 11:18:48 +0300

    admin mods

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,10 +1,23 @@
 class Teacher < ActiveRecord::Base
   has_many :schedules
   has_many :students, :through => :schedules
+  has_many :sections, :through => :schedules, :include => :course, :conditions => ['schedules.period_id in (3,5,7,10,14)']
 
   validates_presence_of :login, :firstName, :lastName, :staffId
 
   def fullName
     [firstName, lastName].join(' ')
+  end
+  
+  def courses_taught
+  	courses = []
+  	self.sections.each do |s|
+  		courses << s.course
+  	end
+  	courses.uniq
+  end
+  
+  def teaches_course? course
+  	courses_taught.include? Course.find_by_name("#{course}")
   end
 end
